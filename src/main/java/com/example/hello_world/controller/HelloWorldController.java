00f1abe8663a7df621c0af65601d11590ebd525a
naveenvilas <naveen.shpy@gmail.com>
2024-08-01 21:16:04 +0530

    Code changes

---
+++ src/main/java/com/example/hello_world/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.hello_world.controller;
+//package com.example.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
--- src/test/java/com/example/hello_world/HelloWorldApplicationTests.java
+++ src/test/java/com/example/hello_world/HelloWorldApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hello.world;
+package com.example.hello_world;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WorldApplicationTests {
+class HelloWorldApplicationTests {
 
 	@Test
 	void contextLoads() {
